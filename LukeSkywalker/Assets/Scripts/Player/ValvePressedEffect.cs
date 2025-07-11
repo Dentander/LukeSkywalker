6e1332b517b78dedf4e67049e818d5d267bf0a6f
AlexanderOsharov <sashosharov@gmail.com>
2025-07-11 14:03:36 +0300

    Not Trash

--- LukeSkywalker/Assets/Scripts/Player/ValvePressedEffect.cs
+++ LukeSkywalker/Assets/Scripts/Player/ValvePressedEffect.cs
@@ -1,34 +1,37 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Image))]
 [RequireComponent(typeof(Valve))]
 public class ValvePressedEffect : MonoBehaviour {
-    private SpriteRenderer _renderer;
-    private Valve _valve;
+  private Valve _valve;
 
-    private void Start() {
-        _renderer = GetComponent<SpriteRenderer>();
-        _valve = GetComponent<Valve>();
+  [SerializeField]
+  private Sprite _pressedSprite;
+  [SerializeField]
+  private Sprite _defaultSprite;
+
+  private void Start() {
+    _valve = GetComponent<Valve>();
+  }
+
+  public void ApplyEffect() {
+    GetComponent<Image>().sprite = _pressedSprite;
+    StartCoroutine(DisableEffect());
+  }
+
+  private IEnumerator DisableEffect() {
+    yield return new WaitForSeconds(0.6f);
+    GetComponent<Image>().sprite = _defaultSprite;
+  }
+
+  private void OnEnable() {
+    if (_valve == null) {
+      _valve = GetComponent<Valve>();
     }
+    _valve.OnPress += ApplyEffect;
+  }
 
-    public void ApplyEffect() {
-        _renderer.color = Color.black;
-        StartCoroutine(DisableEffect());
-    }
-
-    private IEnumerator DisableEffect() {
-        yield return new WaitForSeconds(0.6f);
-        _renderer.color = Color.white;
-    }
-
-    private void OnEnable() {
-        if (_valve ==  null) {
-            _renderer = GetComponent<SpriteRenderer>();
-            _valve = GetComponent<Valve>();
-        }
-        _valve.OnPress += ApplyEffect;
-    }
-
-    private void OnDisable() => _valve.OnPress -= ApplyEffect;
+  private void OnDisable() => _valve.OnPress -= ApplyEffect;
 }
